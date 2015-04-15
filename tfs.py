76ee8d650a44f3fd8ee6eda63cd6e0ffe1120bcc
jwprillaman <jwprillaman@gmail.com>
2015-04-15 11:48:40 -0500

    now wrapping up zip in torrent file

--- tfs.py
+++ tfs.py
@@ -1,15 +1,20 @@
 import os
 import sys
 
+TRACKER = "udp://tracker.openbittorrent.com:80"
+
 if len(sys.argv) != 3:
   print("Usage: tfs [-save|-get] [directory |file] [password]") 
   exit()
 
+#Saves encrypted home directory to torrent file
 def save( saveFile ):
+  #working dir of the program
   workDir = os.path.dirname(os.path.abspath(__file__))
-  print("zip -e -P " + sys.argv[2] + " " +  workDir + "/temp/" + "homeZip " + "-r "  +workDir + "/home")
+  #zips up ./home dir and encrypts with given password. Saves to ./temp
   os.system("zip -e -P " + sys.argv[2] + " " +  workDir + "/temp/" + "homeZip " + "-r "  +workDir + "/home") 
-
+  #creats torrent file using TRACKER saves to temp
+  os.system("transmission-create -o " + workDir + "/temp/homeTor.torrent " + "-t " + TRACKER + " " + workDir + "/temp/homeZip.zip")
 def get( getFiles ):
   print('get')
 
