8e0776cb1510d09160e622ca4af072615f8d2fc6
jwprillaman <jwprillaman@gmail.com>
2015-04-20 20:41:55 -0500

    now seeding correctly

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 # Torrent File System
 ### Create By: James Prillaman
-James left his account logged in, in the faner Linux lab... 
-U got hacked
+
+##Description
+
+TFS is a python application that allows users to encrypt and save a working directory. Then TFS will create a torrent for this directory so as that it can be shared accross multiple computers.
+
+
+## Dependencies
+
+transmission
+```
+sudo apt-get install transmission-cli
+
+```
+
+zip
+
+```
+sudo apt-get install zip
+
+```
+
+unzip
+
+```
+sudo apt-get install unzip
+
+```
+
+##Usage
+
+```
+python tfs.py -save [password]
+
+```
+Save current working directory of tfs/home to zip file and encrypt with given password. creates torrent for Home directory in tfs/downloads
--- tfs.py
+++ tfs.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
-TRACKER = "udp://tracker.openbittorrent.com:80"
+#TRACKER = "udp://tracker.openbittorrent.com:80"
+TRACKER = "udp://open.demonii.com:1337"
+TOR_DIR = "/var/lib/transmission-daemon/downloads"
 
 if len(sys.argv) != 3:
   print("Usage: tfs [-save|-get] [directory |file] [password]") 
@@ -12,9 +14,12 @@ def save( saveFile ):
   #working dir of the program
   workDir = os.path.dirname(os.path.abspath(__file__))
   #zips up ./home dir and encrypts with given password. Saves to ./temp
-  os.system("zip -e -P " + sys.argv[2] + " " +  workDir + "/temp/" + "homeZip " + "-r "  +workDir + "/home") 
+  os.system("zip -e -P " + sys.argv[2] + " " +  workDir + "/downloads/" + "homeZip " + "-r "  +workDir + "/home") 
   #creats torrent file using TRACKER saves to temp
-  os.system("transmission-create -o " + workDir + "/temp/homeTor.torrent " + "-t " + TRACKER + " " + workDir + "/temp/homeZip.zip")
+  os.system("transmission-create -o " + workDir + "/downloads/homeTor.torrent " + "-t " + TRACKER + " " + workDir + "/downloads/homeZip.zip")
+  #Add torrent to transmission for seeding
+  os.system("transmission-remote --add " + workDir + "/downloads/homeTor.torrent")
+
 def get( getFiles ):
   print('get')
 
